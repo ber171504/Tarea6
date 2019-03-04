5f89f4a389f97794710c7e6d7adca091470462b5
Andres <ber171504@uvg.edu.gt>
2019-03-03 20:43:33 -0800

    Falta profiler

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,10 @@
+/*
+*Andres Berthet 171504
+* Hoja de Trabajo 6
+* 3 de Marzo de 2019
+* Algoritmos y estructura de deatos
+* */
+
 import java.util.AbstractMap;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
@@ -5,25 +12,55 @@ import java.util.TreeMap;
 import java.util.Scanner;
 import java.io.*;
 public class Main {
-    public static void main(String [] args){
+    public static void main(String [] args) throws IOException {
         System.out.println("Cual de los 3 mapas desea usar \n  1, HashMap \n  2, LinkedHashMap \n  3, TreeMap \n ingrese el numero");
-        Scanner input  = new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
         int map = input.nextInt();
         MapFacotry factory = new MapFacotry();
-        AbstractMap Mapa = factory.getmap(map);
-        System.out.println(type(Mapa));
+        AbstractMap<String, String> Mapa = factory.getmap(map);// se usa el factory para saber cual de los tres mapoas se quiere usar
 
+        System.out.println("ingrese donde se localiza el archivo con las cartas: ");// bufferReader codigo basado de los ejemplos encontrados en la pagina : https://www.geeksforgeeks.org/different-ways-reading-text-file-java/
         String fileName = input.next();
         File file = new File(fileName);
         FileReader fr = new FileReader(file);
         BufferedReader br = new BufferedReader(fr);
         String line;
-        while((line = br.readLine()) != null){
+        while ((line = br.readLine()) != null) {
             //process the line
-            split(line,"|");
-            Mapa.put(split.0,split.1);
+            String[] parts = line.split("[|]");
+            Mapa.put(parts[0], parts[1]);
+
         }
-
-
+        int continuar =0;
+        while (continuar == 0) {
+            System.out.println("Que deseas hacer \n    1. Ver todas las cartas disponibles \n    2. Agregar una carta por su nombre al deck \n    3. Ver el deck \n    4. Salir");
+            int opcion = input.nextInt();
+            HashMap<String, String> Deck = new HashMap<String, String>();
+            if (opcion == 1) {
+            for (AbstractMap.Entry<String, String> entry : Mapa.entrySet())
+            {
+                System.out.println(entry.getKey() + "/" + entry.getValue());
+            }
+            }
+            if (opcion == 2) {
+                System.out.println("Ingrese el nombre de su carta ");
+                input.nextLine();
+                String carta = input.nextLine();
+                carta = carta + "|" + Mapa.get(carta);
+                System.out.println(carta);
+                String[] parts = carta.split("[|]");
+                Deck.put(parts[0], parts[1]);
+            }
+            if (opcion == 3) {
+                for (AbstractMap.Entry<String, String> entry : Deck.entrySet())
+                {
+                    System.out.println(entry.getKey() + "/" + entry.getValue());
+                }
+            }
+            if (opcion == 4 ){
+                continuar= 1;
+            }
+        }
     }
 }
+//C:\Users\ticoa\OneDrive\Desktop\Cosas\EstructuraDeDatos\Tarea6\cartas
--- src/MapFacotry.java
+++ src/MapFacotry.java
@@ -1,18 +1,24 @@
+/*
+ *Andres Berthet 171504
+ * Hoja de Trabajo 6
+ * 3 de Marzo de 2019
+ * Algoritmos y estructura de deatos
+ * */
 import java.util.AbstractMap;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.TreeMap;
 
-public class MapFacotry {
+public class MapFacotry {// Factory que se usa pa la creacion de los diferentes tipos de mapas
     public AbstractMap getmap(int map){
         if (map==1){
-            return new HashMap<String,String>();
+            return new HashMap<String,String>();// se crea un Hash map
         }
         else if (map==2){
-            return new LinkedHashMap<String,String>();
+            return new LinkedHashMap<String,String>(); // se crea un linked has map
         }
         else if (map==3){
-            return new TreeMap<String,String>();
+            return new TreeMap<String,String>();// se cre un tree map
         }
         return null;
     }
