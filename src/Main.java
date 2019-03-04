e9c6926545b66da3a5c798100e07871209bbb429
Andres <ber171504@uvg.edu.gt>
2019-03-03 21:41:10 -0800

    2

--- src/Main.java
+++ src/Main.java
@@ -27,21 +27,22 @@ public class Main {
         String line;
         while ((line = br.readLine()) != null) {
             //process the line
-            String[] parts = line.split("[|]");
-            Mapa.put(parts[0], parts[1]);
+            String[] parts = line.split("[|]");// se separa el string que da el buffer reader
+            Mapa.put(parts[0], parts[1]);// se guarda la carta en el mapa con su key y value adecuado
 
         }
-        AbstractMap<String, String> Deck = new HashMap<String, String>();
+        AbstractMap<String, String> Deck = new HashMap<String, String>();// se crea un hash map para guardar las cartas personales del usuario
         int continuar =0;
         while (continuar == 0) {
             System.out.println("Que deseas hacer \n    1. Ver todas las cartas disponibles \n    2. Agregar una carta por su nombre al deck \n    3. Ver el deck \n    4. Salir");
-            int opcion = input.nextInt();
-            if (opcion == 1) {
+            int opcion = input.nextInt();// se le pide al usuario que decida que va a hacer
+            if (opcion == 1) {// para la opcion se imprimen todas las cartas que contiene el archivo
             for (AbstractMap.Entry<String, String> entry : Mapa.entrySet())
             {
                 System.out.println(entry.getKey() + "|" + entry.getValue());
             }
                 System.out.println("Desea ver el deck de manera ordenada por tipo? \n ingrese el numero \n    1. si \n    2. no ");
+            // se le pregunta si desea ver las cartas en orden y se imprimen en orden con los forloops siguientes
                 int orden = input.nextInt();
                 if (orden==1){
                     for (AbstractMap.Entry<String, String> entry : Mapa.entrySet())
@@ -58,21 +59,21 @@ public class Main {
                     }
                 }
             }
-            if (opcion == 2) {
+            if (opcion == 2) {// En la opcion dos se le pide al usuario que ingrese el nombre de una carta y esta se busca en el mapa para se imprime con su key y value para despues ser agregado al deck del usuario
                 System.out.println("Ingrese el nombre de su carta ");
                 input.nextLine();
                 String carta = input.nextLine();
-                carta = carta + "|" + Mapa.get(carta);
+                carta = carta + "|" + Mapa.get(carta); // imprime la carta que se busca y se le asigna su valor segun la ingformacion que se encuentra en el mapa
                 System.out.println(carta);
                 String[] partes = carta.split("[|]");
-                Deck.put(partes[0], partes[1]);
+                Deck.put(partes[0], partes[1]);// se agrega esta carta al mapa o deck personal del usauraio
             }
-            if (opcion == 3) {
+            if (opcion == 3) {// opcion 3 se imprimen las cartas que tiene el usuario
                 for (AbstractMap.Entry<String, String> entry : Deck.entrySet())
                 {
                     System.out.println(entry.getKey() + "|" + entry.getValue());
                 }
-                System.out.println("Desea ver el deck de manera ordenada por tipo? \n ingrese el numero \n    1. si \n    2. no ");
+                System.out.println("Desea ver el deck de manera ordenada por tipo? \n ingrese el numero \n    1. si \n    2. no "); // esta imprime las cartas en el deck del usuario ordnadas por tipo
                 int orden = input.nextInt();
                 if (orden==1){
                     for (AbstractMap.Entry<String, String> entry : Deck.entrySet())
@@ -89,7 +90,7 @@ public class Main {
                     }
                 }
             }
-            if (opcion == 4 ){
+            if (opcion == 4 ){// opcion 4 es para salir del while loop
                 continuar= 1;
             }
         }
